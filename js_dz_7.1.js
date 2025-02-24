e3b86ce635f5ef7705c18df0df1665ccbc74ef30
alexalochka <veysts@gmail.com>
2025-02-24 17:38:54 +0200

    second

--- js_dz_7.1.js
+++ js_dz_7.1.js
@@ -2,10 +2,17 @@ let alice = "Won232derful, Joyful, Happiness, Time, Homework, Task, Apple, bigH5
 let pattern = /[B-Zb-z0-9]{6,}/g;
 console.log(alice.match(pattern))
 
-// let alice = "Wonderful, Joyful, Happiness, Time, Task, Apple, mamamamam, korona"
-// let pattern = /[^Aa]{6,}/g;
+
+
+// // Ще хотіла зробити за методом виключення
+// let alice = "Won232derful, Joyful, Happiness, Time, Homework, Task, Apple, bigH546t4orse, korona, vegetable"
+// let pattern = /[^Aa\s]{6,}/g;
 // console.log(alice.match(pattern))
 
+
+// // Тут повне слово (в кінці вашого конспекту):
 // let str = "Won232derful, Joyful, Happiness, Time, Homework, Task, Apple, bigH546t4orse, korona, vegetable";
 // let pattern2 = /\b[^Aa\s]{6,}\b/g;
 // console.log(str.match(pattern2));
+
+//Єдине ще питання, якщо ми робимо виключекння [^] - ми вже в цих квадратних дужках не можемо вказати те, що НЕ потрібно виключати?
